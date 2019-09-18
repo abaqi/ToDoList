43b4e8c8b37a1627e047dc19eb0227af785c5305
Araiz Baqi <abaqi@my.uri.edu>
2019-09-17 22:06:13 -0500

    starting category page

---
+++ TodoList/Controllers/CategoryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CategoryViewController.swift
+//  TodoList
+//
+//  Created by MacBook Pro on 17/09/2019.
+//  Copyright © 2019 abc. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - TableView Datasource Methods
+    //MARK: - Data Manipulation Methods
+    //MARK: - Add New Categories
+
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+
+    //MARK: - TableView Datasource Methods
+    
+    //MARK: - Tableview Delegate Methods
+        //Leave this for now
+        //This will refer to what will happen if one of the categorys is selected
+    
+    //MARK: - Data Manipulation Methods
+    
+}
